6f3fb80e5b83238f3ba4f81c7f0b3782b924b595
Your name <Mahima Subramanian>
2017-09-21 11:43:53 -0500

    added 3 references

--- src/components/References.js
+++ src/components/References.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import Mailto from 'react-mailto';
 
 class References extends Component {
   render() {
 
      return (
        <div>
-         <p>Past employers</p>
-         <p></p>
+         <p><a href="http://gameofthrones.wikia.com/wiki/Daenerys_Targaryen" to="/gameofthrones">Mother of Dragons</a></p>
+         <p><a href="http://pixar.wikia.com/wiki/Dory" to="/dory">Dory</a></p>
+         <Mailto email="mahima.subramanian@gmail.com" obfuscate={true}>J.Patera, former manager</Mailto>
        </div>
      );
-   }
- }
+  }
+}
 
 
 export default References;
