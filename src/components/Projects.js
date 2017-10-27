01c1a2b74c24a13357e20e9e62b2b7d0aa6225f2
Your name <Mahima Subramanian>
2017-10-27 12:52:05 -0500

    added two projects with links to github to projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import calculator from '../images/calculator.png';
+import clearpath from '../images/clearPath.png';
+
+
 
 export default class Projects extends Component {
 
   render() {
 
      return (
-       <div>
-         <p>Projects</p>
-         
+       <div className="portfolio">
+
+       <div className="project">
+         <h3> Calculator </h3>
+         <p className="project-info"> A simple calcultor with Johnny 5 as your mascot</p>
+         <img src={calculator} className="portfolio-image"/>
+         <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
+         <div className="project-links">
+           <a>Live Link </a>
+           <a href="https://github.com/Smahima/calculatorAlgorithmsPractice"> Code</a>
+         </div>
+        </div>
+
+        <div className="project">
+          <h3> Calculator </h3>
+          <p className="project-info"> A venue rating site focused on Accessibility</p>
+          <img className="portfolio-image"/>
+          <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
+          <div className="project-links">
+            <a>Live Link </a>
+            <a href="https://github.com/Smahima/clearPath"> Code</a>
+          </div>
+         </div>
+
+         <div className="project">
+           <h3> Calculator </h3>
+           <p className="project-info"> A user directory of robot characters</p>
+           <img src={clearpath} className="portfolio-image"/>
+           <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
+           <div className="project-links">
+             <a>Live Link </a>
+             <a href="https://github.com/Smahima/clearPath"> Code</a>
+           </div>
+          </div>
+
        </div>
      );
    }
