8090e46b89ad6c784b9923a564e92921cd7a3cab
Your name <Mahima Subramanian>
2017-11-01 14:39:01 -0500

    removed footer, moved contact info and icons to header, moved name & title to left

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -13,8 +13,25 @@ class BaseLayout extends Component {
       <div className="main">
 
        <div className="header">
+
+       <div className="name">
           <h1> Mahima Subramanian</h1>
           <h3> Front End Web Developer</h3>
+      </div>  
+
+        <div className="contact">
+          <div className="email">
+            <p>How to get in touch with me: mahima.subramanian[at]gmail.com</p>
+            <p><a id="emaillink" href="mailto:mahima.subramanian@gmail.com?subject=Ciao Mahima!">This link opens a blank email</a></p>
+          </div>
+
+          <div className="social-media">
+            <a id="linkedin" href="https://www.linkedin.com/in/msubramanian/"><img src={LinkedIn} alt="LinkedIn Icon" height="50px"/> </a>
+            <a id="github" href="https://github.com/Smahima"><img src={GitHub} alt="GitHub Icon" height="50px"/> </a>
+            <a id="medium" href="https://medium.com/@mahimaS"><img src={Medium} alt="Medium" height="50px"/> </a>
+          </div>
+        </div>
+
        </div>
 
         <div className="nav">
@@ -23,22 +40,7 @@ class BaseLayout extends Component {
           <button><NavLink activeClassName="selected" className="nav-link" to="/resume">Résumé</NavLink></button>
         </div>
 
-
         {this.props.children}
-
-        <footer className="contact">
-
-        <div className="email">
-          <p>How to get in touch with me: mahima.subramanian[at]gmail.com</p>
-          <p><a id="emaillink" href="mailto:mahima.subramanian@gmail.com?subject=Ciao Mahima!">This link opens a blank email</a></p>
-        </div>
-
-         <div className="social-media">
-           <a id="linkedin" href="https://www.linkedin.com/in/msubramanian/"><img src={LinkedIn} alt="LinkedIn Icon" height="50px"/> </a>
-           <a id="github" href="https://github.com/Smahima"><img src={GitHub} alt="GitHub Icon" height="50px"/> </a>
-           <a id="medium" href="https://medium.com/@mahimaS"><img src={Medium} alt="Medium" height="50px"/> </a>
-         </div>
-        </footer>
 
       </div>
 
