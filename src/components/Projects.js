594558bd139b4478c896d7ef9e44358a413bb2fc
Your name <Mahima Subramanian>
2017-11-02 12:22:34 -0500

    put projects and text into two columns

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,58 +12,65 @@ export default class Projects extends Component {
 
      return (
 
-       <div className="portfolio">
 
 
-       <div className="project">
-         <h3>Bent Creek Ceramics</h3>
-         <img src={ceramics} className="portfolio-image"/>
-         <p className="project-details">Site for ceramics store</p>
-         <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
-         <div className="project-links">
-           <a href="https://ceramicscreekbent.surge.sh">Live Link</a>
-           <a href="https://github.com/Smahima/bentCreekCeramics.git"> Code</a>
+       <div id="container">
+
+         <div id="img">
+           <h3>Bent Creek Ceramics</h3>
+           <img src={ceramics} className="portfolio-image"/>
          </div>
-        </div>
 
-       <div className="project">
-          <h3>Calculator</h3>
-         <img src={calculator} className="portfolio-image"/>
-           <p className="project-details">This project tasked us to create a simple calculator able to handle basic arithmetic.</p>
+         <div id="content">
+           <p className="project-details">Site for ceramics store</p>
            <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
-         <div className="project-links">
-           <a href="https://johnny5.surge.sh">Live Link </a>
-           <a href="https://github.com/Smahima/calculatorAlgorithmsPractice"> Code</a>
-         </div>
+          <a href="https://ceramicscreekbent.surge.sh">Live Link</a>
+          <a href="https://github.com/Smahima/bentCreekCeramics.git"> Code</a>
         </div>
 
-        <div className="project">
-          <h3>Play List</h3>
-           <img src={playList} className="portfolio-image"/>
-          <p className="project-details">A party playlist to which anyone can add songs as well as comments</p>
-          <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript, React</p>
-          <div className="project-links">
-            <a href="https://songstoplay.surge.sh">Live Link </a>
-            <a href="https://github.com/Smahima/addToPlayListReactApp.git"> Code</a>
+          <div id="img">
+            <h3>Calculator</h3>
+            <img src={calculator} className="portfolio-image"/>
           </div>
-         </div>
 
-        <div className="inProgress">
-          <a id="under-construction"><h2>Under Construction</h2></a>
+          <div id="content">
+            <p className="project-details">This project tasked us to create a simple calculator able to handle basic arithmetic.</p>
+            <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript</p>
+            <a href="https://johnny5.surge.sh">Live Link </a>
+            <a href="https://github.com/Smahima/calculatorAlgorithmsPractice"> Code</a>
+          </div>
 
-          <div className="project">
-            <h3> ClearPath</h3>
-            <img src={clearpath} className="portfolio-image"/>
+          <div id="img">
+           <h3>Play List</h3>
+           <img src={playList} className="portfolio-image"/>
+          </div>
+
+           <div id="content">
+              <p className="project-details">A party playlist to which anyone can add songs as well as comments</p>
+              <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript, React</p>
+              <a href="https://songstoplay.surge.sh">Live Link </a>
+              <a href="https://github.com/Smahima/addToPlayListReactApp.git"> Code</a>
+            </div>
+
+          <div id="img">
+            <div>
+              <a id="under-construction"><h2>Under Construction</h2></a>
+            </div>
+
+            <div>
+              <h3> ClearPath</h3>
+              <img src={clearpath} className="portfolio-image"/>
+            </div>
+
+          </div>
+
+          <div id="content">
             <p className="project-details"> A venue rating site focused on Accessibility.</p>
             <p className="technology"> <span className="bold"> Technology used:</span> CSS, HTML5, JavaScript, React</p>
-          <div className="project-links">
-              <a href="https://github.com/Smahima/clearPath">Code</a>
+            <a href="https://github.com/Smahima/clearPath">Code</a>
           </div>
-          </div>
-         </div>
+        </div>
 
-
-       </div>
      );
    }
  }
