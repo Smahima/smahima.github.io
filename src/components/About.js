ea6d3c2160579c03b1d790b0b212327412dc4501
Your name <Mahima Subramanian>
2017-11-02 08:57:23 -0500

    re-formatted text on about page

--- src/components/About.js
+++ src/components/About.js
@@ -22,14 +22,15 @@ export default class About extends Component {
             <p>Moved to Austin from New England (could not take those winters anymore),</p>
             <p>over two years ago and now happy to call ATX home.</p><br></br>
 
-            <p>I am a front end web developer, recently completed a fullstack program at The Iron Yard where I learned React.</p>
-            <p>I devote my time to creating engaging, attractive, and user-friendly sites.I have showcased a few</p>
-            <p>projects that are complete nd one that is in progress. Similarly, this site itself is constantly </p>
-            <p>updated as I learn new functionality and ways in which to style. </p>
+            <p>I am a front end web developer, recently completed a fullstack program at The Iron Yard where</p>
+            <p>I learned React. I devote my time to creating engaging, attractive, and user-friendly sites. I have  </p>
+            <p>showcased a few projects that are complete and one that is in progress. My personal site </p>
+            <p> itself is constantly updated as I learn new functionality and ways in which to style. </p>
             <br></br>
 
-            <p>My personal mission statement is to live life with purpose and intention, in everything I do, personal and professional. I found my way
-               to coding through diverse career paths from social work to project management.</p>
+            <p>My personal mission statement is to live life with purpose and intention, in everything</p>
+            <p>I do, personal and professional. I found my way to coding through diverse career paths</p>
+            <p>from social work to project management.</p>
             <br></br>
 
             <p>When I’m not in front of the terminal, I am reading (my ideal home is a library),</p>
